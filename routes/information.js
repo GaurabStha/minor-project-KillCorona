86b8afab5bc189d90696a2e66998454dac482bae
Gaurab Shrestha <gaurabian111@gmail.com>
2021-03-04 09:51:33 +0545

    fix added

--- routes/information.js
+++ routes/information.js
@@ -8,17 +8,14 @@ router.get('/information', function(req, res, next) {
 
 router.get('/information', function(req, res, next) {
     res.render('information');
-    next();
 });
 
 router.get('/preventions', function(req, res, next) {
     res.render('preventions');
-    next();
 });
 
 router.get('/treatments', function(req, res, next) {
     res.render('treatments');
-    next();
 });
 
 module.exports = router;
